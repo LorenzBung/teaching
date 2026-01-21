812d7ff6908348c83b446c879e40dd15fd93c17b
Lorenz Bung <lorenz.bung@googlemail.com>
2026-01-21 14:38:12 +0100

    Add FTE1/Dictionaries (LP)

---
+++ Informatik/FTE1/Strukturierte Programmierung/Dictionaries/maus_lsg.py
@@ -0,0 +1,136 @@
+text_romanian = "Povesti vesele si cu tâlc astazi cu Andre în telecabina cu cârtita în zapada cu noul Snowboard al Johanna distractie la ferma si evident cu soricelul elefantul si rata"
+
+ro_de = {
+    "aer": "Luft",
+    "al": "von",
+    "animal": "Tier",
+    "an": "Jahr",
+    "apa": "Wasser",
+    "arbore": "Baum",
+    "astazi": "heute",
+    "autor": "Autor",
+
+    "baiat": "Junge",
+    "biblioteca": "Bibliothek",
+    "bine": "gut",
+    "bogat": "reich",
+    "bucurie": "Freude",
+
+    "calm": "ruhig",
+    "cârtita": "Maulwurf",
+    "carte": "Buch",
+    "casa": "Haus",
+    "copac": "Baum",
+    "creativ": "kreativ",
+    "culoare": "Farbe",
+    "cu": "mit",
+    "curios": "neugierig",
+
+    "deschis": "offen",
+    "desene": "Zeichnungen",
+    "distractie": "Spaß",
+    "doi": "zwei",
+    "drum": "Weg",
+    "dulce": "süß",
+
+    "echilibru": "Gleichgewicht",
+    "echipa": "Team",
+    "elefantul": "Elefant",
+    "energie": "Energie",
+    "evident": "natürlich",
+
+    "familie": "Familie",
+    "ferma": "Farm",
+    "fidel": "treu",
+    "floare": "Blume",
+    "frumos": "schön",
+
+    "galben": "gelb",
+    "generos": "großzügig",
+    "gradina": "Garten",
+    "greu": "schwer",
+
+    "harta": "Karte",
+
+    "iarna": "Winter",
+    "idee": "Idee",
+    "important": "wichtig",
+    "în": "in",
+    "insula": "Insel",
+    "istorie": "Geschichte",
+
+    "joc": "Spiel",
+
+    "la": "die",
+    "liber": "frei",
+    "liniste": "Ruhe",
+    "lumina": "Licht",
+    "luna": "Mond",
+
+    "mare": "groß",
+    "mâncare": "Essen",
+    "mic": "klein",
+    "modest": "bescheiden",
+    "munte": "Berg",
+
+    "negru": "schwarz",
+    "noapte": "Nacht",
+    "nor": "Wolke",
+    "noul": "neuem",
+
+    "obiect": "Gegenstand",
+    "om": "Mensch",
+    "oras": "Stadt",
+
+    "paine": "Brot",
+    "padure": "Wald",
+    "Povesti": "Geschichten",
+    "prieten": "Freund",
+    "puternic": "stark",
+
+    "rapid": "schnell",
+    "rata": "Ente",
+    "rau": "schlecht",
+    "rece": "kalt",
+    "roman": "rumänisch",
+    "rosu": "rot",
+
+    "scoala": "Schule",
+    "si": "und",
+    "simplu": "einfach",
+    "sincer": "ehrlich",
+    "soare": "Sonne",
+    "soricelul": "Maus",
+    "stilou": "Füller",
+
+    "tâlc": "Bedeutung",
+    "tanar": "jung",
+    "telecabina": "Seilbahn",
+    "timp": "Zeit",
+    "tren": "Zug",
+
+    "usa": "Tür",
+    "usor": "leicht",
+
+    "vant": "Wind",
+    "varf": "Gipfel",
+    "verde": "grün",
+    "vesele": "glücklich",
+    "viata": "Leben",
+
+    "zambet": "Lächeln",
+    "zapada": "Schnee",
+    "zi": "Tag"
+}
+
+
+# Schreiben Sie hier Ihren Code
+
+uebersetzung = ""
+for wort in text_romanian.split(" "):
+    if wort in ro_de:
+        uebersetzung += ro_de[wort]
+    else:
+        uebersetzung += wort
+    uebersetzung += " "
+print(uebersetzung)
